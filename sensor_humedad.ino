3e5abcfaee7f7fb8609b3bd79925853b24613327
telecompucp <46438448+telecompucp@users.noreply.github.com>
2025-02-04 17:43:38 -0500

    Update sensor_humedad.ino

--- sensor_humedad.ino
+++ sensor_humedad.ino
@@ -1,24 +1,20 @@
 int sensor_pin = A0; // conectar el sensor de humedad al pin analógico A0 del nodeMCU
-int humedad;
-
+int humedad, sensor;
 
 void setup() {
-pinMode(5,OUTPUT);
+pinMode(5,OUTPUT); //pin D2 del nodeMCU
 Serial.begin(9600); // Inicia la comunicación serial
 Serial.println("Iniciando la detección de humedad...");
 delay(2000);
-
 }
 
 void loop() {
-
-humedad = analogRead(sensor_pin); //Los valores varian entre 0 - 1023 para tierra húmeda a seca
-humedad  = map(humedad,1024,410,0,100); //Escalamiento de valores 
+sensor = analogRead(sensor_pin); //Los valores varian entre 0 - 1023 para tierra húmeda a seca
+humedad  = map(sensor,410,1023,100,0); //Escalamiento de valores 
 Serial.print("Humedad de la tierra: ");
 Serial.print(humedad);
 Serial.println("%");
-Serial.println(analogRead(sensor_pin));
-
+Serial.println(sensor);
 delay(1000);
 
 if (humedad>=50)
